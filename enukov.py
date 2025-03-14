882fa9f8f5f2bbfeaa7bfc35c151b8297b8de75e
Sergey Enukov <40084509-sergeyenukov@users.noreply.replit.com>
2025-03-14 08:35:40 +0000

    Итоговые изменения

--- enukov.py
+++ enukov.py
@@ -39,7 +39,6 @@
 #print(tfame)
 #print(tfame2)
 
-
 # #Контейнер визуализации
 #from matplotlib import pyplot as plt
 # plt.plot(tfame['Y'], tfame['C_ost_lst'], label = 'Am')
@@ -97,7 +96,6 @@
 #print(tfame)
 #print(tfame2)
 
-
 # from matplotlib import pyplot as plt
 #plt.plot(tfame['Y'], tfame['C_ost_lst'], label = 'Am')
 #plt.plot(tfame2['Y'], tfame2['C_ost_lst_2'], label = 'Am_2')
@@ -119,7 +117,6 @@
 #ax.axis("equal")
 #plt.show()
 
-
 #table1 = list(zip(Y, Am_lst))
 #table2 = list(zip(Y, Am_lst_2))
 #tfame = pd.DataFrame(table1, columns = ['Y', 'Am_lst'])
@@ -128,38 +125,34 @@
 #plt.bar(tfame['Y'], tfame['Am_lst'])
 #plt.show()
 
-
 #сосед все правильно сделал
-
-
-
-
 
 # Вариант 7
 # Первый способ
 from sympy import *
+
 k, T, C, L = symbols('k C T L')
 
-C_ost=70000
-Am_lst=[]
-C_ost_lst=[]
+C_ost = 70000
+Am_lst = []
+C_ost_lst = []
 for i in range(8):
-  Am = (C-L)/T
-  C_ost -= Am.subs({C: 70000, T:8, L:0})
-  Am_lst.append(round(Am.subs({C: 70000, T:8, L:0}), 2))
+  Am = (C - L) / T
+  C_ost -= Am.subs({C: 70000, T: 8, L: 0})
+  Am_lst.append(round(Am.subs({C: 70000, T: 8, L: 0}), 2))
   C_ost_lst.append(round(C_ost, 2))
 print('Am_lst:', Am_lst)
 print('C_ost_lst:', C_ost_lst)
 
 #2 способ
-Aj=0
-C_ost=70000
-Am_lst_2=[]
-C_ost_lst_2=[]
+Aj = 0
+C_ost = 70000
+Am_lst_2 = []
+C_ost_lst_2 = []
 for i in range(8):
-  Am = k * 1/T * (C - Aj)
-  C_ost -= Am.subs({C: 70000, T:8, k:2})
-  Am_lst_2.append(round(Am.subs({C: 70000, T:8, k:2}), 2))
+  Am = k * 1 / T * (C - Aj)
+  C_ost -= Am.subs({C: 70000, T: 8, k: 2})
+  Am_lst_2.append(round(Am.subs({C: 70000, T: 8, k: 2}), 2))
   Aj += Am
   C_ost_lst_2.append(round(C_ost, 2))
 print('Am_lst_2:', Am_lst_2)
@@ -167,15 +160,15 @@ print('C_ost_lst_2:', C_ost_lst_2)
 
 #Контейнер табличного вывода
 import pandas as pd
+
 Y = range(1, 9)
 table1 = list(zip(Y, C_ost_lst, Am_lst))
 table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
-tfame = pd.DataFrame(table1, columns = ['Y', 'C_ost_lst', 'Am_lst'])
-tfame2 = pd.DataFrame(table2, columns = ['Y', 'C_ost_lst_2', 'Am_lst_2'])
+tfame = pd.DataFrame(table1, columns=['Y', 'C_ost_lst', 'Am_lst'])
+tfame2 = pd.DataFrame(table2, columns=['Y', 'C_ost_lst_2', 'Am_lst_2'])
 
 print(tfame)
 print(tfame2)
-
 
 from matplotlib import pyplot as plt
 #plt.plot(tfame['Y'], tfame['C_ost_lst'], label = 'Am')
@@ -186,7 +179,17 @@ vals = Am_lst
 labels = list(range(1, 9))
 explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
 fig, ax = plt.subplots()
-ax.pie(vals, labels=labels, autopct='%1.1f%%', shadow=True, explode=explode, wedgeprops={'lw':1, 'ls':'--', 'edgecolor': "k"}, rotatelabels=True)
+ax.pie(vals,
+       labels=labels,
+       autopct='%1.1f%%',
+       shadow=True,
+       explode=explode,
+       wedgeprops={
+           'lw': 1,
+           'ls': '--',
+           'edgecolor': "k"
+       },
+       rotatelabels=True)
 ax.axis("equal")
 plt.show()
 
@@ -197,7 +200,6 @@ plt.show()
 #ax.pie(vals, labels=labels, autopct='%1.1f%%', shadow=True, explode=explode, wedgeprops={'lw':1, 'ls':'--', 'edgecolor': "k"}, #rotatelabels=True)
 #ax.axis("equal")
 #plt.show()
-
 
 #table1 = list(zip(Y, Am_lst))
 #table2 = list(zip(Y, Am_lst_2))
